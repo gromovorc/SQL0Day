b926a46069672d92a6b97ec5f3fe734f27c81464
gromovorc <arkady-gromov@mail.ru>
2023-03-30 20:08:41 +0300

    -commit
    5 из 5

---
+++ TR_Basket_insert_update.sql
@@ -0,0 +1,18 @@
+CREATE TRIGGER trg_CalculateDiscountValue
+ON dbo.Basket
+AFTER INSERT
+AS
+BEGIN
+  UPDATE b
+  SET DiscountValue = CASE
+                        WHEN c.Count >= 2 THEN b.Value * 0.05
+                        ELSE 0
+                      END
+  FROM dbo.Basket b
+  INNER JOIN (
+    SELECT ID_SKU, COUNT(*) AS Count
+    FROM inserted
+    GROUP BY ID_SKU
+  ) c ON b.ID_SKU = c.ID_SKU
+  WHERE b.ID IN (SELECT ID FROM inserted)
+END;
