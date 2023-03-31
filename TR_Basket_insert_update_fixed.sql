e8a901bc9003cec071aa6e21618bbb1cf60ce560
gromovorc <arkady-gromov@mail.ru>
2023-03-31 19:36:34 +0300

    -commit
    Исправленный вариант 5го задания

---
+++ TR_Basket_insert_update_fixed.sql
@@ -0,0 +1,21 @@
+CREATE TRIGGER TR_Basket_insert_update
+ON dbo.Basket
+AFTER INSERT
+AS
+BEGIN
+
+WITH cteCount AS (
+SELECT ID_SKU, COUNT(*) AS Count
+FROM inserted
+GROUP BY ID_SKU
+)
+
+UPDATE b
+SET DiscountValue = CASE
+WHEN c.Count >= 2 THEN b.Value * 0.05
+ELSE 0
+END
+FROM dbo.Basket b
+JOIN cteCount c ON b.ID_SKU = c.ID_SKU
+WHERE b.ID IN (SELECT ID FROM inserted)
+END;
